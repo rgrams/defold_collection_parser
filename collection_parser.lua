275d35ad5fdcf6313afbb641db73a9cd621909c4
rossgrams <ross.grams@gmail.com>
2019-10-04 18:43:01 -0400

    Add license bit to module.

--- collection_parser.lua
+++ collection_parser.lua
@@ -1,6 +1,9 @@
 
 --		Copyright (c) 2019 Ross Grams
 
+-- This library is free software; you can redistribute it and/or modify it
+-- under the terms of the MIT license. See LICENSE for details.
+
 local M = {}
 
 local IGNORE_KEYS = {
